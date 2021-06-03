0999624f555d6a1d101da8952710df42d0455b87
Serhii <gubserj@gmail.com>
2021-06-03 13:51:27 +0300

    task_8 featured

--- tasks/task_8/main.go
+++ tasks/task_8/main.go
@@ -2,21 +2,17 @@ package main
 
 import "fmt"
 
-func fibonachi(n uint) (f uint) {
-	if n == 0 {
-		f = 0
-	} else if n == 1 {
-		f = 1
-	} else {
-		fiboArray := []uint{0, 1}
-		for i := 2; uint(i) <= n; i++ {
-			f = fiboArray[i-1] + fiboArray[i-2]
-			fiboArray = append(fiboArray, f)
-		}
+func fibonachi(n uint) uint {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		return fibonachi(n-1) + fibonachi(n-2)
 	}
-	return f
 
 }
 func main() {
-	fmt.Println(fibonachi(10))
+	fmt.Println(fibonachi(0))
 }
