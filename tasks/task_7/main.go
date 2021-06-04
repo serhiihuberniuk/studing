380575b6e8e5616a7da76f4472a1809982547875
Serhii <gubserj@gmail.com>
2021-06-04 10:10:25 +0300

    task_7 featured

--- tasks/task_7/main.go
+++ tasks/task_7/main.go
@@ -12,11 +12,11 @@ type house struct {
 }
 
 func main() {
-	var home house
-	home.wall.A = 13 // not sure its the best way to value the fields
-	home.roof.A = 8
-	home.roof.B = 8
-	home.roof.C = 6
+	home := house{
+		roof:   geometry.Triangle{8, 8, 6},
+		ground: geometry.Rectangle{5, 15},
+		wall:   geometry.Square{13},
+	}
 
 	squareToPaint := home.wall.Square()*4 + home.roof.Square()*2
 	fmt.Println(squareToPaint)
