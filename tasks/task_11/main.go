b54145e436d53a6ef5ccdd7192838126bcdc166f
Serhii <gubserj@gmail.com>
2021-06-08 18:31:51 +0300

    task_11 featured with GracefulShutdown

--- tasks/task_11/main.go
+++ tasks/task_11/main.go
@@ -1,50 +1,103 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 )
 
-func addZeros(s string, out chan string) {
-	s = s + "000"
-	out <- s
+func addZeros(ctx context.Context, in chan string, out chan string) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			s := <-in
+			s = s + "000"
+			out <- s
+		}
+
+	}
 }
-func convertToInt(in chan string, out chan int) {
-	s := <-in
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		out <- 1
-		return
-	} else {
-		out <- i
+func convertToInt(ctx context.Context, in chan string, out chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			s := <-in
+			i, err := strconv.Atoi(s)
+			if err == nil {
+				out <- i
+			}
+		}
 	}
 }
 
-func convertToString(in chan int, out chan string) {
+func convertToString(ctx context.Context, in chan int, out chan string) {
 
-	i := <-in
-	if i == 1 {
-		out <- ""
-		return
-	} else {
-		s := strconv.Itoa(i)
-		out <- s
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			i := <-in
+			s := strconv.Itoa(i)
+			out <- s
+		}
 	}
 }
 
 func main() {
-	stringArray := []string{"90", "23", "hh", "123", "34", "4452", "23", "123", "000", "12", "34"}
-	var ch1 = make(chan string)
-	var ch2 = make(chan int)
-	var ch3 = make(chan string)
-	for _, v := range stringArray {
-		go addZeros(v, ch1)
-		go convertToInt(ch1, ch2)
-		go convertToString(ch2, ch3)
-		s := <-ch3
-		if s != "" {
-			fmt.Println(s)
-		}
-	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+
+	setupGracefulShutdown(cancel)
+
+	stringArray := []string{"90", "23", "30", "123", "34", "4452", "23", "123", "000", "12", "34"}
+	ch := make(chan string)
+	ch1 := make(chan string)
+	ch2 := make(chan int)
+	ch3 := make(chan string)
+
+	go addZeros(ctx, ch, ch1)
+	go convertToInt(ctx, ch1, ch2)
+	go convertToString(ctx, ch2, ch3)
+
+	go func() {
+		for _, v := range stringArray {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				ch <- v
+			}
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				fmt.Println(<-ch3)
+			}
+		}
+	}()
+	<-ctx.Done()
+	close(ch)
+	close(ch1)
+	close(ch2)
+	close(ch3)
+}
+func setupGracefulShutdown(stop func()) {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-signalChannel
+		stop()
+	}()
 }
