4b88298a22964cb7d7b14f3b56c2bdacdff3b6d5
Serhii <gubserj@gmail.com>
2021-06-08 11:53:30 +0300

    task_11 featured

--- tasks/task_11/main.go
+++ tasks/task_11/main.go
@@ -1,43 +1,50 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
-	"strings"
 )
 
-func addZeros(s string, ch1 chan string) {
+func addZeros(s string, out chan string) {
 	s = s + "000"
-	ch1 <- s
+	out <- s
 }
-func convertToInt(ch1 chan string, ch2 chan int) {
-	s := <-ch1
+func convertToInt(in chan string, out chan int) {
+	s := <-in
 	i, err := strconv.Atoi(s)
 	if err != nil {
-		err = errors.New("wrong string")
+		out <- 1
+		return
+	} else {
+		out <- i
 	}
-	ch2 <- i
 }
 
-func convertToString(ch2 chan int) {
-	i := <-ch2
-	s := strconv.Itoa(i)
-	fmt.Println(s)
+func convertToString(in chan int, out chan string) {
+
+	i := <-in
+	if i == 1 {
+		out <- ""
+		return
+	} else {
+		s := strconv.Itoa(i)
+		out <- s
+	}
 }
 
 func main() {
-	text := "90 23 239 123 34 4452 23 123 34 12 34"
-	var stringArray []string
-	stringArray = strings.Fields(text)
+	stringArray := []string{"90", "23", "hh", "123", "34", "4452", "23", "123", "000", "12", "34"}
+	var ch1 = make(chan string)
+	var ch2 = make(chan int)
+	var ch3 = make(chan string)
 	for _, v := range stringArray {
-		var ch1 = make(chan string)
-		var ch2 = make(chan int)
 		go addZeros(v, ch1)
 		go convertToInt(ch1, ch2)
-		go convertToString(ch2)
+		go convertToString(ch2, ch3)
+		s := <-ch3
+		if s != "" {
+			fmt.Println(s)
+		}
 	}
-	var input string   // stole this decision from golang-book
-	fmt.Scanln(&input) // do i had to use mutex?
 
 }
