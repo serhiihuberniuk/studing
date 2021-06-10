05b7482c54dc955d1c6ae407b49b1f3a972a5e93
Serhii <gubserj@gmail.com>
2021-06-10 16:29:40 +0300

    task_12 check

---
+++ tasks/task_12/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+)
+
+type printer interface {
+	Print([]int)
+}
+
+func printArray(arr []int, p printer) {
+
+	var wg sync.WaitGroup
+	n := int(math.Ceil(float64(len(arr)) / 5))
+
+	srcChan := make(chan int)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			var newArr []int
+			for i := 0; i < 5; i++ {
+				v, ok := <-srcChan
+				if ok == false {
+					break
+				}
+				newArr = append(newArr, v)
+			}
+			p.Print(newArr)
+			wg.Done()
+		}()
+	}
+	for _, v := range arr {
+		srcChan <- v
+	}
+	close(srcChan)
+	wg.Wait()
+
+}
+
+type myPrinter struct{}
+
+func (myPrinter) Print(arr []int) {
+	fmt.Println(arr)
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	var m myPrinter
+
+	printArray(arr, m)
+
+}
