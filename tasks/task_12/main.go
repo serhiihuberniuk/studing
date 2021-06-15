be3f7de48c836ce5bc8c90f3aaab4067974b6124
Serhii <gubserj@gmail.com>
2021-06-15 17:48:44 +0300

    task_12 fetured

--- tasks/task_12/main.go
+++ tasks/task_12/main.go
@@ -15,27 +15,34 @@ func printArray(arr []int, p printer) {
 	var wg sync.WaitGroup
 	n := int(math.Ceil(float64(len(arr)) / 5))
 
-	srcChan := make(chan int)
+	srcChan := make(chan int, 5)
+	wg.Add(n)
+	go func() {
+		for _, v := range arr {
+			srcChan <- v
 
+		}
+		close(srcChan)
+	}()
 	for i := 0; i < n; i++ {
-		wg.Add(1)
+
 		go func() {
-			var newArr []int
+			newArr := make([]int, 0, 2)
+
 			for i := 0; i < 5; i++ {
+
 				v, ok := <-srcChan
 				if ok == false {
 					break
 				}
 				newArr = append(newArr, v)
 			}
+
 			p.Print(newArr)
 			wg.Done()
 		}()
 	}
-	for _, v := range arr {
-		srcChan <- v
-	}
-	close(srcChan)
+
 	wg.Wait()
 
 }
@@ -47,7 +54,7 @@ func (myPrinter) Print(arr []int) {
 }
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
 	var m myPrinter
 
 	printArray(arr, m)
